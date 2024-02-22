504f9a7dbf7ecf0ed371f29aaa70c6a1220f361b
LUIGI DOS SANTOS BERNARDO <luigi_bernardo@estudante.sesisenai.org.br>
2024-02-21 21:48:41 -0300

    Aula 4

--- Quarta Fase/Front End (Rafael)/Receita/Script.js
+++ Quarta Fase/Front End (Rafael)/Receita/Script.js
@@ -1,3 +1,27 @@
+
 function CriarReceita(){
-    alert('Deu')
+    const receitas = document.getElementById("DivReceita");
+
+    const divTitulo = document.createElement("div");
+    const inputTitulo = document.createElement("input");
+
+    const divIngrediente = document.createElement("div");
+    const inputNome = document.createElement("input");
+    const inputQntd = document.createElement("input");
+    const inputUnid = document.createElement("input");
+    
+   
+/*--------------------------------------------- */
+    divTitulo.id = "divTitulo";
+    receitas.appendChild(divTitulo);
+
+    inputTitulo.placeholder = "Título da Receita";
+    inputTitulo.id = "inputTitulo"
+    divTitulo.appendChild(inputTitulo);
+/*--------------------------------------------- */
+    divIngrediente.id = "divIngrediente";
+    receitas.appendChild(divIngrediente);
+
+    
 }
+document.getElementById("BotaoCriarReceita").disabled=true
